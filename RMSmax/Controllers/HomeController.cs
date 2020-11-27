112788535fe3e0eb6a784980d2f84b42afacc11a
Jakub Wadas <j.wadas1999@gmail.com>
2020-11-27 20:08:56 +0100

    FakeArticleRepository

--- RMSmax/Controllers/HomeController.cs
+++ RMSmax/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RMSmax.Models;
 
 namespace RMSmax.Controllers
 {
     public class HomeController : Controller
     {
+        private IArticleRepository articlesRepo;
+        public HomeController(IArticleRepository artsRepo)
+        {
+            articlesRepo = artsRepo;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(articlesRepo.Articles);
         }
     }
 }
---
+++ RMSmax/Models/FakeArticleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMSmax.Models
+{
+    public class FakeArticleRepository : IArticleRepository
+    {
+        public IQueryable<Article> Articles => new List<Article> {
+            new Article { Title = "Tytuł 1", Content = "Treść" },
+            new Article { Title = "Tytuł 2", Content = "Treść" },
+            new Article { Title = "Tytuł 3", Content = "Treść" },
+            new Article { Title = "Tytuł 4", Content = "Treść" },
+            new Article { Title = "Tytuł 5", Content = "Treść" },
+            new Article { Title = "Tytuł 6", Content = "Treść" }
+        }.AsQueryable<Article>();
+    }
+}
