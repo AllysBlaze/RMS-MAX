76aa38de05c8c625942ec3635e4e0a31b7507b4d
Jakub Wadas <j.wadas1999@gmail.com>
2020-11-28 23:39:08 +0100

    Mechanizm stronnicowania

--- RMSmax/Models/Article.cs
+++ RMSmax/Models/Article.cs
@@ -7,6 +7,7 @@ namespace RMSmax.Models
 {
     public class Article
     {
+        public uint Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         //...
