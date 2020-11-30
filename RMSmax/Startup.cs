4a31fdf0304b9a5d3ad5d85685e3befb85b14253
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2020-11-30 19:28:39 +0100

    Baza danych bez funkcjonalności

--- RMSmax/Startup.cs
+++ RMSmax/Startup.cs
@@ -7,16 +7,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using RMSmax.Models;
+using Microsoft.EntityFrameworkCore;
+using RMSmax.Data;
+
 
 namespace RMSmax
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RMSContext>(opts => opts.UseSqlServer("name=RMSDB"));
             services.AddTransient<IArticleRepository, FakeArticleRepository>();
             services.AddControllersWithViews();
         }
