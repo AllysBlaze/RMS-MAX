785538dd58be75452c952783308ddbe11b7044d4
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2020-12-03 20:41:59 +0100

    Employees fake repo

---
+++ RMSmax/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMSmax.Models
+{
+    public class Employee
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Degree { get; set; }
+        public string Department { get; set; }//Katedra
+        public string Function { get; set; }
+        public string Position { get; set; } //stanowisko
+        public string Room { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string Timetable { get; set; }
+    }
+}
---
+++ RMSmax/Models/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMSmax.Models
+{
+    interface IEmployeeRepository
+    {
+        IQueryable<Employee> Employees{ get; }
+    }
+}
