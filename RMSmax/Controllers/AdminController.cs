010e1f0da4b30f2ce542f3bfc97da9cc29d92075
Jakub Wadas <j.wadas1999@gmail.com>
2020-12-11 18:39:10 +0100

    Kontroler administratora - wstępny zarys

---
+++ RMSmax/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using RMSmax.Models;
+using RMSmax.Models.ViewModels;
+
+namespace RMSmax.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IArticleRepository articlesRepo;
+        private IEmployeeRepository employeesRepo;
+        private IStudentsTimetableRepository studentsTimetableRepo;
+        private ISubjectRepository subjectRepo;
+        private Faculty facultyInfo;
+        private IWebHostEnvironment Environment;
+        public AdminController(IArticleRepository artsRepo, IEmployeeRepository empRepo, IStudentsTimetableRepository timetableRepo, ISubjectRepository subjectRepo, IWebHostEnvironment env)
+        {
+            articlesRepo = artsRepo;
+            employeesRepo = empRepo;
+            studentsTimetableRepo = timetableRepo;
+            this.subjectRepo = subjectRepo;
+            facultyInfo = Faculty.FacultyInstance is null ? new Faculty(env.WebRootPath) : Faculty.FacultyInstance;
+            Environment = env;
+        }
+
+        [HttpGet]
+        public IActionResult AdminIndex()
+        {
+            return View(new MainViewModel() { FacultyCourses = facultyInfo.Courses });
+        }
+
+        [HttpGet]
+        public IActionResult EditCourse()
+        {
+            return View(new MainViewModel() { FacultyCourses = facultyInfo.Courses });
+        }
+
+        [HttpPost]
+        public IActionResult EditCourse(Course course, IList<StudentsTimetable> timetables)
+        {
+            //zapis danych
+            return RedirectToAction(nameof(AdminIndex));
+        }
+    }
+}
