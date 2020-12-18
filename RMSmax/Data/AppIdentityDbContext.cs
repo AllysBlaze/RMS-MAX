e790832da3cda2e5b8add13888609c37d971d768
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2020-12-18 19:36:21 +0100

    Dodany pakiet identity

---
+++ RMSmax/Data/AppIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RMSmax.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RMSmax.Data
+{
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options) { }
+    }
+}
+
