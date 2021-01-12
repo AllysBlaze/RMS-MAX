c3cd04123f868055f5598a7cd480f5a453e16b0c
Krzysztof Grzesica <frivoloussav@gmail.com>
2021-01-12 19:54:19 +0100

    Dodanie podglądu haseł

---
+++ RMSmax/wwwroot/js/showPass.js
@@ -0,0 +1,23 @@
+﻿$(function () { 
+                $("#show1").mousedown(function () {
+                $("#old").attr("type", "text");
+                });
+
+                $("#show1").mouseup(function () {
+                $("#old").attr("type", "password");
+                });
+
+                $("#show2").mousedown(function () {
+                $("#new").attr("type", "text");
+                });
+                $("#show2").mouseup(function () {
+                $("#new").attr("type", "password");
+                });
+
+                $("#show3").mousedown(function () {
+                $("#new2").attr("type", "text");
+                });
+                $("#show3").mouseup(function () {
+                $("#new2").attr("type", "password");
+                });
+});
---
+++ RMSmax/wwwroot/js/showPassUserAdd.js
@@ -0,0 +1,16 @@
+﻿$(function () { 
+                $("#show1").mousedown(function () {
+                    $("#new").attr("type", "text");
+                });
+
+                $("#show1").mouseup(function () {
+                    $("#new").attr("type", "password");
+                });
+
+                $("#show2").mousedown(function () {
+                $("#new2").attr("type", "text");
+                });
+                $("#show2").mouseup(function () {
+                $("#new2").attr("type", "password");
+                });
+});
