765745081334fed681e5205188d981309d34910b
Krzysztof Grzesica <frivoloussav@gmail.com>
2021-01-14 14:13:47 +0100

    Dodanie podglądu hasła do ViewLogin

---
+++ RMSmax/wwwroot/js/showPassLogin.js
@@ -0,0 +1,9 @@
+﻿$(function () { 
+                $("#show").mousedown(function () {
+                $("#pass").attr("type", "text");
+                });
+
+                $("#show").mouseup(function () {
+                $("#pass").attr("type", "password");
+                });
+});
