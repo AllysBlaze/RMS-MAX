20ed46d9a54293dfc99fde484eca4278ec75120f
Krzysztof Grzesica <frivoloussav@gmail.com>
2021-01-14 14:30:20 +0100

    Naprawa podglądu hasła

--- RMSmax/wwwroot/js/showPass.js
+++ RMSmax/wwwroot/js/showPass.js
@@ -1,23 +1,32 @@
 ﻿$(function () { 
-                $("#show1").mousedown(function () {
-                $("#old").attr("type", "text");
-                });
 
-                $("#show1").mouseup(function () {
-                $("#old").attr("type", "password");
-                });
+    $("#show1").mousedown(function () {
+        $("#old").attr("type", "text");
+    });
+    $("#show1").mouseup(function () {
+        $("#old").attr("type", "password");
+    });
+    $("#show1").mouseleave(function () {
+        $("#old").attr("type", "password");
+    });
 
-                $("#show2").mousedown(function () {
-                $("#new").attr("type", "text");
-                });
-                $("#show2").mouseup(function () {
-                $("#new").attr("type", "password");
-                });
+    $("#show2").mousedown(function () {
+        $("#new").attr("type", "text");
+    });
+    $("#show2").mouseup(function () {
+        $("#new").attr("type", "password");
+    });
+    $("#show2").mouseleave(function () {
+        $("#new").attr("type", "password");
+    });
 
-                $("#show3").mousedown(function () {
-                $("#new2").attr("type", "text");
-                });
-                $("#show3").mouseup(function () {
-                $("#new2").attr("type", "password");
-                });
+    $("#show3").mousedown(function () {
+        $("#new2").attr("type", "text");
+    });
+    $("#show3").mouseup(function () {
+        $("#new2").attr("type", "password");
+    });
+    $("#show3").mouseleave(function () {
+        $("#new2").attr("type", "password");
+    });
 });
